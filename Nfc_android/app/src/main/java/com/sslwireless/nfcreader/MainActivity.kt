98ac21070cedf163b027638d089da34a77e79775
AscEmon <sayedemon53@gmail.com>
2022-04-18 22:21:05 +0600

    fix: method channel chache issue resolved in android

--- Nfc_android/app/src/main/java/com/sslwireless/nfcreader/MainActivity.kt
+++ Nfc_android/app/src/main/java/com/sslwireless/nfcreader/MainActivity.kt
@@ -1,41 +1,81 @@
 package com.sslwireless.nfcreader
 
+//import android.os.Bundle
+//import android.widget.Button
+//import android.widget.TextView
+//import androidx.appcompat.app.AppCompatActivity
+//import io.flutter.embedding.android.FlutterActivity
+//
+//
+//class MainActivity : AppCompatActivity() {
+//    private lateinit var counterLabel: TextView
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//
+//        setContentView(R.layout.activity_main)
+//        counterLabel = findViewById(R.id.counter_label)
+//        val button = findViewById<Button>(R.id.launch_button)
+//
+//
+//
+//        button.setOnClickListener {
+////            val intent =
+////                FlutterActivity
+////              .withNewEngine()
+////                .initialRoute("/")
+////                .build(this)
+//////                FlutterActivity
+//////                    .withCachedEngine(ENGINE_ID)
+//////                    .build(this);
+////            startActivity(intent)
+////            startActivity(
+////                FlutterActivity.createDefaultIntent(this)
+//            startActivity(
+//                FlutterActivity
+//                    .withCachedEngine(ENGINE_ID)
+//                    .build(this)
+//            )
+//
+//        }
+//        super.onCreate(savedInstanceState)
+//    }
+//
+////    override fun onResume() {
+////        super.onResume()
+////        val app = MyApplication()
+////        counterLabel.text = "Current count: ${app.count}"
+////    }
+//}
+
+
+
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import io.flutter.embedding.android.FlutterActivity
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var counterLabel: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
 
         counterLabel = findViewById(R.id.counter_label)
 
         val button = findViewById<Button>(R.id.launch_button)
 
-
-
         button.setOnClickListener {
-            val intent =
-                FlutterActivity
-              .withNewEngine()
-                .initialRoute("/")
+            val intent = FlutterActivity
+                .withCachedEngine(ENGINE_ID)
                 .build(this)
-//                FlutterActivity
-//                    .withCachedEngine(ENGINE_ID)
-//                    .build(this);
             startActivity(intent)
         }
     }
 
     override fun onResume() {
         super.onResume()
-        val app = MyApplication()
+        val app = application as MyApplication
         counterLabel.text = "Current count: ${app.count}"
     }
 }
